ec736ed65e6b6c2ba97df9586967a206989d23dc
jsericksk <jsericksk@gmail.com>
2024-01-06 18:12:57 -0300

    Adicionados exemplos de Images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,9 @@ dependencies {
 
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/kproject/composejourney/presentation/content/components1/Image.kt
@@ -0,0 +1,133 @@
+package com.kproject.composejourney.presentation.content.components1
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.kproject.composejourney.R
+import com.kproject.composejourney.presentation.theme.MyAppTheme
+
+@Composable
+private fun Icon1() {
+    Icon(
+        imageVector = Icons.Default.Person,
+        contentDescription = "Person icon",
+        tint = Color.Blue
+    )
+}
+
+@Composable
+private fun Icon2() {
+    Icon(
+        painter = painterResource(id = R.drawable.baseline_person_24),
+        contentDescription = "Person icon",
+        tint = Color.Red
+    )
+}
+
+@Composable
+private fun Image1() {
+    Image(
+        painter = painterResource(id = R.drawable.img_nature),
+        contentDescription = "Image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.size(width = 200.dp, height = 150.dp)
+    )
+}
+
+@Composable
+private fun ImageWeb() {
+    AsyncImage(
+        model = "https://img.lermanga.org/Y/yotsuba-to/capitulo-09/12.jpg",
+        contentDescription = "Image"
+    )
+}
+
+@Composable
+private fun Image2() {
+    val borderWidth = 4.dp
+    Image(
+        painter = painterResource(id = R.drawable.img_nature),
+        contentDescription = "Image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(150.dp)
+            .border(
+                border = BorderStroke(borderWidth, Color.Red),
+                shape = CircleShape
+            )
+            .clip(CircleShape)
+    )
+}
+
+@Composable
+private fun Image3() {
+    Image(
+        painter = painterResource(id = R.drawable.img_nature),
+        contentDescription = "Image",
+        contentScale = ContentScale.Crop,
+        colorFilter = ColorFilter.tint(color = Color.Green, blendMode = BlendMode.Darken),
+        modifier = Modifier.size(width = 200.dp, height = 150.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview1() {
+    MyAppTheme {
+        PaddingForPreview(padding = 4.dp) {
+            Icon1()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview2() {
+    MyAppTheme {
+        PaddingForPreview(padding = 4.dp) {
+            Icon2()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview3() {
+    MyAppTheme {
+        Image1()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview4() {
+    MyAppTheme {
+        PaddingForPreview(padding = 4.dp) {
+            Image2()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview5() {
+    MyAppTheme {
+        Image3()
+    }
+}
