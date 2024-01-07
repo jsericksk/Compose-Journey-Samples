4050b70a96389d8627e4b669b8f705b79bdd21bf
jsericksk <jsericksk@gmail.com>
2024-01-07 00:17:28 -0300

    Adicionados exemplos de Slider

---
+++ app/src/main/java/com/kproject/composejourney/presentation/content/components1/Slider.kt
@@ -0,0 +1,74 @@
+package com.kproject.composejourney.presentation.content.components1
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.RangeSlider
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kproject.composejourney.presentation.theme.MyAppTheme
+import kotlin.math.roundToInt
+
+@Composable
+private fun Slider1() {
+    var sliderPosition by remember { mutableFloatStateOf(40f) }
+    Column {
+        Slider(
+            value = sliderPosition,
+            onValueChange = { position ->
+                sliderPosition = position
+            },
+            steps = 4,
+            valueRange = 0f..100f
+        )
+        Text(
+            text = sliderPosition.roundToInt().toString(),
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+private fun RangeSlider() {
+    var sliderPosition by remember { mutableStateOf(0f..100f) }
+    Column {
+        RangeSlider(
+            value = sliderPosition,
+            onValueChange = { position ->
+                sliderPosition = position
+            },
+            valueRange = 0f..100f
+        )
+        Text(
+            text = "Start: ${sliderPosition.start.roundToInt()} / End: ${sliderPosition.endInclusive.roundToInt()}",
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview1() {
+    MyAppTheme {
+        PaddingForPreview {
+            Slider1()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview2() {
+    MyAppTheme {
+        PaddingForPreview {
+            RangeSlider()
+        }
+    }
+}
