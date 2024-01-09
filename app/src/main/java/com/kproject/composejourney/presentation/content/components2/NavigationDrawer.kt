a9a68d89e1a61d1a72a5819f641c97986ef82852
jsericksk <jsericksk@gmail.com>
2024-01-09 02:31:43 -0300

    Adicionados exemplos de Navigation Drawer

---
+++ app/src/main/java/com/kproject/composejourney/presentation/content/components2/NavigationDrawer.kt
@@ -0,0 +1,174 @@
+package com.kproject.composejourney.presentation.content.components2
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kproject.composejourney.R
+import com.kproject.composejourney.presentation.theme.MyAppTheme
+import kotlinx.coroutines.launch
+
+sealed class NavDrawerItem(
+    @DrawableRes val iconResId: Int,
+    @StringRes val titleResId: Int,
+) {
+    data object Home : NavDrawerItem(R.drawable.baseline_home_24, R.string.home)
+    data object PrivacyPolicy : NavDrawerItem(R.drawable.baseline_privacy_tip_24, R.string.privacy_policy)
+    data object SavedFiles : NavDrawerItem(R.drawable.baseline_save_24, R.string.saved_files)
+    data object ShareApp : NavDrawerItem(R.drawable.baseline_share_24, R.string.share_app)
+}
+
+@Composable
+private fun MyNavigationDrawer(
+    drawerState: DrawerState,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val items = listOf(
+        NavDrawerItem.Home,
+        NavDrawerItem.PrivacyPolicy,
+        NavDrawerItem.SavedFiles,
+        NavDrawerItem.ShareApp,
+    )
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Spacer(Modifier.height(22.dp))
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    modifier = Modifier.padding(start = 30.dp)
+                )
+                Spacer(Modifier.height(22.dp))
+                items.forEach { item ->
+                    NavigationDrawerItem(
+                        icon = {
+                            Icon(
+                                painter = painterResource(id = item.iconResId),
+                                contentDescription = stringResource(id = item.titleResId)
+                            )
+                        },
+                        label = {
+                            Text(
+                                text = stringResource(id = item.titleResId)
+                            )
+                        },
+                        selected = (item == NavDrawerItem.Home),
+                        onClick = {
+                            coroutineScope.launch { drawerState.close() }
+                            when (item) {
+                                is NavDrawerItem.Home -> {}
+                                is NavDrawerItem.PrivacyPolicy -> {}
+                                is NavDrawerItem.SavedFiles -> {}
+                                is NavDrawerItem.ShareApp -> {}
+                            }
+                        },
+                        modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                    )
+                }
+            }
+        },
+        modifier = modifier,
+        content = content
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun HomeScreen() {
+    val coroutineScope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    MyNavigationDrawer(drawerState = drawerState) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer
+                    ),
+                    title = {
+                        Text("TopAppBar")
+                    },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = {
+                                coroutineScope.launch {
+                                    drawerState.open()
+                                }
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = "Menu"
+                            )
+                        }
+                    }
+                )
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add"
+                    )
+                }
+            }
+        ) { paddingValues ->
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .padding(paddingValues)
+                    .fillMaxSize()
+            ) {
+                Text(
+                    text = "Conteúdo da tela",
+                    fontSize = 24.sp
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview1() {
+    MyAppTheme {
+        HomeScreen()
+    }
+}
