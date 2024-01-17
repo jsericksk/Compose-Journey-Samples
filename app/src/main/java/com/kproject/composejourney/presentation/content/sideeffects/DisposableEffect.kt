69039a4b4335f9fe055a29ea0494dc7ce138412c
jsericksk <jsericksk@gmail.com>
2024-01-17 00:02:31 -0300

    Adicionado exemplo de DisposableEffect

---
+++ app/src/main/java/com/kproject/composejourney/presentation/content/sideeffects/DisposableEffect.kt
@@ -0,0 +1,60 @@
+package com.kproject.composejourney.presentation.content.sideeffects
+
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+import com.kproject.composejourney.presentation.theme.MyAppTheme
+
+@Composable
+private fun LifecycleObserver(
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+    onStart: () -> Unit,
+    onStop: () -> Unit,
+) {
+    // Atualiza com segurança os lambdas atuais quando um novo é fornecido
+    val currentOnStart by rememberUpdatedState(onStart)
+    val currentOnStop by rememberUpdatedState(onStop)
+
+    // Se `lifecycleOwner` mudar, descarta e reinicia o efeito
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_START) {
+                currentOnStart()
+            } else if (event == Lifecycle.Event.ON_STOP) {
+                currentOnStop()
+            }
+        }
+
+        // Adiciona o observer ao ciclo de vida
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        // Quando o efeito deixar a composição, remove o observer
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview1() {
+    MyAppTheme {
+
+    }
+}
