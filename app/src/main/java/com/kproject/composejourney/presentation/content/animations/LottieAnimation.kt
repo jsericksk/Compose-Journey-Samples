61f79859062940f94bdacd7751d06f57da5ad7e6
jsericksk <jsericksk@gmail.com>
2024-01-19 05:46:41 -0300

    Adicionados exemplos de animações Lottie e outras

---
+++ app/src/main/java/com/kproject/composejourney/presentation/content/animations/LottieAnimation.kt
@@ -0,0 +1,48 @@
+package com.kproject.composejourney.presentation.content.animations
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.kproject.composejourney.R
+import com.kproject.composejourney.presentation.theme.PreviewTheme
+
+@Composable
+private fun LoadingAnimation() {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loading_animation))
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever
+    )
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        LottieAnimation(
+            composition = composition,
+            progress = { progress },
+            modifier = Modifier.size(200.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview1() {
+    PreviewTheme(darkTheme = false) {
+        LoadingAnimation()
+    }
+}
---
+++ app/src/main/java/com/kproject/composejourney/presentation/content/animations/Others.kt
@@ -0,0 +1,89 @@
+package com.kproject.composejourney.presentation.content.animations
+
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kproject.composejourney.presentation.theme.PreviewTheme
+
+@Composable
+private fun Info() {
+    var expanded by remember { mutableStateOf(false) }
+    val textPadding by animateDpAsState(
+        targetValue = if (expanded) 14.dp else 4.dp,
+        animationSpec = spring(
+            dampingRatio = 3f,
+            stiffness = Spring.StiffnessLow
+        ),
+        label = "textPadding"
+    )
+    val iconRotationAngle by animateFloatAsState(
+        targetValue = if (expanded) 180f else 0f,
+        animationSpec = spring(
+            dampingRatio = 2f,
+            stiffness = Spring.StiffnessMediumLow
+        ),
+        label = "iconRotationAngle"
+    )
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        Text(
+            text = "Jetpack Compose Journey!",
+            color = Color.White,
+            modifier = Modifier
+                .background(
+                    color = Color.Black,
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .padding(textPadding)
+        )
+        Spacer(Modifier.height(24.dp))
+        Button(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowDown,
+                contentDescription = null,
+                modifier = Modifier.rotate(iconRotationAngle)
+            )
+            Text(
+                text = if (expanded) "Diminuir padding" else "Aumentar padding"
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview1() {
+    PreviewTheme(darkTheme = false) {
+        Info()
+    }
+}
