a304d22b3cf86bab9e37cdfc4a023d92e8e72f9b
jsericksk <jsericksk@gmail.com>
2024-01-19 19:10:54 -0300

    Adicionado novo exemplo de Text

--- app/src/main/java/com/kproject/composejourney/presentation/content/components/Text.kt
+++ app/src/main/java/com/kproject/composejourney/presentation/content/components/Text.kt
@@ -1,5 +1,12 @@
 package com.kproject.composejourney.presentation.content.components
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
@@ -17,12 +24,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kproject.composejourney.presentation.theme.MyAppTheme
 
@@ -36,6 +47,36 @@ private fun Text() {
         fontFamily = FontFamily.SansSerif,
         letterSpacing = 4.sp
     )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun LongText() {
+    val longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula" +
+            " eu lectus lobortis condimentum. Aliquam nonummy auctor massa."
+    Column(
+        modifier = Modifier
+            .background(Color.Black)
+            .padding(12.dp)
+    ) {
+        Text(
+            text = longText,
+            color = Color.White,
+            fontSize = 18.sp,
+            fontStyle = FontStyle.Italic,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Spacer(Modifier.height(16.dp))
+        Text(
+            text = longText,
+            color = Color.White,
+            fontSize = 18.sp,
+            fontStyle = FontStyle.Italic,
+            maxLines = 1,
+            modifier = Modifier.basicMarquee()
+        )
+    }
 }
 
 @Composable
@@ -153,9 +194,7 @@ private fun Preview1() {
 @Composable
 private fun Preview2() {
     MyAppTheme {
-        PaddingForPreview {
-            TextField()
-        }
+        LongText()
     }
 }
 
@@ -164,7 +203,7 @@ private fun Preview2() {
 private fun Preview3() {
     MyAppTheme {
         PaddingForPreview {
-            TextField2()
+            TextField()
         }
     }
 }
@@ -174,7 +213,7 @@ private fun Preview3() {
 private fun Preview4() {
     MyAppTheme {
         PaddingForPreview {
-            OutlinedTextField()
+            TextField2()
         }
     }
 }
@@ -182,6 +221,16 @@ private fun Preview4() {
 @Preview(showBackground = true)
 @Composable
 private fun Preview5() {
+    MyAppTheme {
+        PaddingForPreview {
+            OutlinedTextField()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview6() {
     MyAppTheme {
         PaddingForPreview {
             OutlinedTextField2()
